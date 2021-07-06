63ba7c2261202cd74cef156d18819c5f9aa27b1e
Sasa Ivanovic <sasaivanovic11@gmail.com>
2021-07-06 16:10:18 +0200

    druga verzija

--- app/src/main/java/com/example/vezba04/MainActivity.java
+++ app/src/main/java/com/example/vezba04/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.vezba04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+    }
+
+    private void initComponents(){
+        buttonSend = findViewById(R.id.buttonSend);
+
+        buttonSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
